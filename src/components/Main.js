cf86b8e225544c62f039cd52f15fc7d5bf43fb9c
dante-robinson <danterobinson2000@gmail.com>
2022-02-10 15:16:45 -0500

    Finishing hooking up all content and links to Home Page.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
+import env from "react-dotenv";
+import axios from "axios";
 
 const Main = ({ lottery }) => {
   const [totalPrizePool, setTotalPrizePool] = useState(null);
   const [pastNumbers, setWinningNumbers] = useState(null);
   const [timeTillDraw, setTimeTillDraw] = useState({});
+  //Only Dynamic incase re-deployed
+  const [lotteryWallet, setLotteryWallet] = useState(null);
+  const [lotteryAddress, setLotteryAddress] = useState(null);
+  //Set value in advance to avoid error when mapping over array
+  const [previousWinners, setPreviousWinners] = useState([]);
 
   const getTotalPrizePool = async (total) => {
     total = await lottery.methods.totalPrizePool().call();
@@ -43,17 +50,79 @@ const Main = ({ lottery }) => {
     return setTimeTillDraw(timeLeft);
   };
 
+  const getLotteryWallet = async (wallet) => {
+    wallet = await lottery.methods.poolOwner().call();
+    return setLotteryWallet(wallet);
+  };
+
+  const getLotteryAddress = async (address) => {
+    address = await lottery.options.address;
+    return setLotteryAddress(address);
+  };
+
+  const getPreviousWinners = async (winners = []) => {
+    // Contract
+    const address = await lottery.options.address;
+    // Lottery Wallet
+    const wallet = await lottery.methods.poolOwner().call();
+
+    const etherscan =
+      "https://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=" +
+      wallet +
+      "&startblock=10113432&endblock=99999999&page=1&offset=10&sort=asc&apikey=" +
+      env.ETHERSCAN_API;
+
+    axios.get(etherscan).then((response) => {
+      const result = response.data.result;
+      for (let i = 0; i < result.length; i++) {
+        if (
+          result[i].to === "0x01be23585060835e02b77ef475b0cc51aa1e0709" ||
+          result[i].to === "0xb3dccb4cf7a26f6cf6b120cf5a73875b7bbc655b" ||
+          result[i].to === "0xe592427a0aece92de3cdee1f18e0157c05861564" ||
+          result[i].to === "0xb27308f9f90d607463bb33ea1bebb41c27cE5ab6" ||
+          result[i].to === wallet.toLowerCase() ||
+          result[i].to === address.toLowerCase() ||
+          result[i].to === "" ||
+          result[i] === undefined
+        ) {
+        } else {
+          winners.push({
+            to: result[i].to,
+            hash: "https://rinkeby.etherscan.io/tx/" + result[i].hash,
+          });
+        }
+      }
+    });
+    if (winners.length) {
+      return setPreviousWinners((previousWinners) => [winners]);
+    }
+  };
+
   useEffect(() => {
     getTotalPrizePool();
-  }, []);
-  useEffect(() => {
     getWinningNumbers();
+    getLotteryWallet();
+    getLotteryAddress();
+    getPreviousWinners();
   }, []);
+
   useEffect(() => {
     setTimeout(() => {
       getTimeTillDraw();
     }, 1000);
-  });
+  }, [timeTillDraw]);
+
+  const returnPreviousWinners = previousWinners
+    .slice(1, 10)
+    .map((winner, index) => {
+      return (
+        <ul>
+          <li key={index}>
+            <a href={winner.hash}> {winner.to}</a>
+          </li>
+        </ul>
+      );
+    });
 
   return (
     <div className="container">
@@ -99,12 +168,30 @@ const Main = ({ lottery }) => {
         checking the lottery address listed below.
       </p>
 
-      <div className="devHeader ui medium header">Developer Wallet</div>
-      <div className="lotteryHeader ui medium header">Lottery Wallet</div>
-      <div className="contractHeader ui medium header">Contract Wallet</div>
-      <div className="winnerHeader ui medium header">Previous Winners</div>
+      <div className="devHeader">
+        <div className="ui medium header">Developer Wallet</div>
+        <p>0x31F7F75D12248892202aC9CC32DA5dEFc5B09dC9</p>
+      </div>
+      <div className="lotteryHeader">
+        <div className="ui medium header">Lottery Wallet</div>
+        <p>{lotteryWallet}</p>
+      </div>
+      <div className="contractHeader">
+        <div className="ui medium header">Contract Address</div>
+        <p>{lotteryAddress}</p>
+      </div>
+      <div className="winnerHeader">
+        <div className="ui medium header">Previous Winners</div>
+        {previousWinners.length ? (
+          returnPreviousWinners
+        ) : (
+          <p>No previous winners</p>
+        )}
+      </div>
       <p className="ghFooter">
-        Check out the source code on Github
+        <a href="https://github.com/dante-robinson/Crypto-Lottery">
+          Check out the source code on Github
+        </a>
         <i className="ghIcon github icon"></i>
       </p>
     </div>
