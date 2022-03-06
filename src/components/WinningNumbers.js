a9a52f9ee04ac1990a082cd24eebdf8306a4609d
dante-robinson <danterobinson2000@gmail.com>
2022-03-06 17:42:35 -0500

    Add "There hasn't been a draw yet" to Previous Winning Numbers.

--- src/components/WinningNumbers.js
+++ src/components/WinningNumbers.js
@@ -7,7 +7,7 @@ const WinningNumbers = ({ lottery }) => {
   const getWinningNumbers = async () => {
     let numbers = [];
     let draw = await lottery.methods.drawNumber().call();
-    setCurrentDraw(currentDraw);
+    setCurrentDraw(draw);
 
     for (let i = 0; i < 7; i++) {
       numbers[i] = await lottery.methods.winningNumbers(draw, i).call();
@@ -28,11 +28,11 @@ const WinningNumbers = ({ lottery }) => {
   });
 
   const returnNoNumbers = () => {
-    if (currentDraw == 1) {
+    if (pastNumbers[0] == 0) {
       return (
         <p className="text-2xl relative top-5">There hasn't been a draw yet</p>
       );
-    } else {
+    } else if (currentDraw === null) {
       return <p className="text-2xl relative top-5">Loading...</p>;
     }
   };
@@ -40,7 +40,7 @@ const WinningNumbers = ({ lottery }) => {
   return (
     <div className="row-start-3 col-start-3 self-center text-center">
       <h3 className="text-4xl font-extrabold">Previous Winning Numbers</h3>
-      {pastNumbers.length === 7 ? (
+      {pastNumbers.length === 7 && pastNumbers[0] != 0 ? (
         <p>{returnPastNumbers}</p>
       ) : (
         <div>{returnNoNumbers()}</div>
